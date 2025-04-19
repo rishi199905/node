cf7362fa40104c067f1f91f6a50eac0d5f3ea70b
rishikesh bhattalwar <bhattalwarr@gmail.com>
2025-04-19 20:08:19 +0530

    schema

--- src/app.js
+++ src/app.js
@@ -1,105 +1,103 @@
-const express = require('express')
-const connectDb = require('./config/database')
-const User = require('./models/user')
-const app = express()
+const express = require("express");
+const connectDb = require("./config/database");
+const User = require("./models/user");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/signup', async (req, res) => {
-
-    const newUser = new User(req.body)
-    try {
-        const response = await User.findOne({ email : req.body.email})
-        if (response) {
-            res.send("User already exists")
-        }
-    } catch (err) {
-        res.status(400).send("Something went wrong")
+app.post("/signup", async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    const response = await User.findOne({ email: req.body.email });
+    if (response) {
+      res.send("User already exists");
     }
-    try {
-        const response = await newUser.save()
-        res.send({"saved" : response})
-    }
-    catch (error) {
-        res.status(400).send(error)
-    }
-    
-})
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+  try {
+    const response = await newUser.save();
+    res.send({ saved: response });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-app.post('/user', async (req, res) => {
-    try {
-        const response = await User.findOne({ email : req.body.email})
-        if (response) {
-            res.send(response)
-        } else {
-            res.status(404).send("User not found")
-        }
-    } catch (err) {
-        res.status(400).send("Something went wrong")
+app.post("/user", async (req, res) => {
+  try {
+    const response = await User.findOne({ email: req.body.email });
+    if (response) {
+      res.send(response);
+    } else {
+      res.status(404).send("User not found");
     }
-})
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
-
-app.get('/feed', async (req, res) => {
-    try {
-        const response = await User.find({})
-        console.log(response)
-        if (response?.length === 0) {
-            res.status(400).send("No users")
-        } else {
-            res.send(response)
-        }
-    } catch (err) {
-        console.log(err)
-        res.status(404).send(err)
+app.get("/feed", async (req, res) => {
+  try {
+    const response = await User.find({});
+    console.log(response);
+    if (response?.length === 0) {
+      res.status(400).send("No users");
+    } else {
+      res.send(response);
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(404).send(err);
+  }
+});
 
-app.delete('/delete', async (req, res) => {
-    try {
-        const response = await User.findByIdAndDelete(req.body._id)
-        if (response) {
-            res.send({"deleted" : response})
-        }
-    } catch (err) {
-        res.send("something went wrong")
+app.delete("/delete", async (req, res) => {
+  try {
+    const response = await User.findByIdAndDelete(req.body._id);
+    if (response) {
+      res.send({ deleted: response });
     }
-})
+  } catch (err) {
+    res.send("something went wrong");
+  }
+});
 
-app.delete('/deleteByEmail', async (req, res) => {
-    try {
-        
-        const response = await User.findOne({ email : req.body.email})
-        if (response) {
-            try {
-                await User.findOneAndDelete({ email : req.body.email})
-                res.send("deleted")
-            } catch (e) {
-                res.status(500).send("Error while deleting by email")
-            }
-        } else {
-            res.status(404).send("User with email id " + req.body.email + 'not found.')
-        }
-    } catch(err) {
-        res.status(500).send("Error while deleting by email")
+app.delete("/deleteByEmail", async (req, res) => {
+  try {
+    const response = await User.findOne({ email: req.body.email });
+    if (response) {
+      try {
+        await User.findOneAndDelete({ email: req.body.email });
+        res.send("deleted");
+      } catch (e) {
+        res.status(500).send("Error while deleting by email");
+      }
+    } else {
+      res
+        .status(404)
+        .send("User with email id " + req.body.email + "not found.");
     }
-})
+  } catch (err) {
+    res.status(500).send("Error while deleting by email");
+  }
+});
 
-app.patch('/update', async (req, res) => {
-    try {
-        await User.findByIdAndUpdate( req.body._id, req.body)
-        res.send({"updated" : req.body._id})
-    } catch (err) {
-        res.status(500).send("something went wrong")
-    }
-})
+app.patch("/update", async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.body._id, req.body, {runValidators : true});
+    res.send({ updated: req.body._id });
+  } catch (err) {
+    res.status(500).send("something went wrong" + err.message);
+  }
+});
 
-connectDb().then(() => {
-    console.log("connected")
+connectDb()
+  .then(() => {
+    console.log("connected");
     app.listen(8080, () => {
-        console.log("listening")
-    })
-}).catch(() => {
-    console.log("NOT!")
-})
-
+      console.log("listening");
+    });
+  })
+  .catch(() => {
+    console.log("NOT!");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserShema = new mongoose.Schema({
-    firstName: {
-        type: String
+  firstName: {
+    type: String,
+    minLength: 3,
+    maxLength: 15,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 12,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate: (value) => {
+      if (!["male", "female", "others"].includes(value.lowercase())) {
+        throw new Error("Gender not allowed");
+      }
     },
-    lastName: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    }
-})
+  },
+  photoUrl: {
+    type: String,
+    required: true,
+    default:
+      "https://giftolexia.com/wp-content/uploads/2015/11/dummy-profile-200x200.png",
+  },
+  about: {
+    type: String,
+    default: "default about",
+  },
+  skills: {
+    type: [String],
+  },
+}, {timestamps : true});
 
-const User = mongoose.model('User', UserShema)
-module.exports = User
+const User = mongoose.model("User", UserShema);
+module.exports = User;
